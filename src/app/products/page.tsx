fed7a16535441b990c65a5fcfc67ab14f96292ed
Guillermo Camarena <memo.9321@gmail.com>
2024-05-16 19:43:22 -0600

    Product Item Layout

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,20 @@
+import ProductListItem from '../../components/Product/ProductListItem';
 const ProductsList = () => {
   return (
-    <main>
-      <h1>Products List</h1>
-    </main>
+    <div className='
+      grid gap-[10px] 
+      grid-cols-[repeat(1,_1fr)] 
+      sm:grid-cols-[repeat(2,_1fr)] 
+      lg:grid-cols-[repeat(3,_1fr)]
+      xl:grid-cols-[repeat(4,_1fr)]
+    '>
+      <ProductListItem />
+      <ProductListItem />
+      <ProductListItem />
+      <ProductListItem />
+      <ProductListItem />
+      <ProductListItem />
+    </div>
   )
 }
 
---
+++ src/components/Product/ProductListItem/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+const ProductListItem = () => {
+  return (
+    <div className='bg-[#515151] rounded-[10px] overflow-hidden w-full max-w-[500px]'>
+      <div className='relative w-full'>
+        <Image 
+          width={500}
+          height={500}
+          src="https://images.unsplash.com/photo-1550259114-ad7188f0a967"
+          alt=''
+        />
+      </div>
+      <div className='px-[1em] py-[1em] pb-[2em]'>
+        <h2 className='font-bold text-[1.2em] m-0 p-0'>Fries with Ketchup</h2>
+        <span className='block font-bold text-[.75em] text-[#d1d1d1]'>$499</span>
+        <p className='text-[.9em] m-0 p-0'>A classic beef patty topped with lettuce, tomato, and our special sauce, served in a sesame seed bun.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductListItem;
