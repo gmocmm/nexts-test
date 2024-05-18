603dc2c005d4ca0f6f9e060dcce62d4d39cb867e
Guillermo Camarena <memo.9321@gmail.com>
2024-05-17 20:00:47 -0600

    No product message on Cart page

--- src/components/Buttons/MainButton/index.tsx
+++ src/components/Buttons/MainButton/index.tsx
@@ -1,13 +1,13 @@
 type MainButtonProps = {
   text: string, 
-  handler: () => void
+  handler?: () => void
 }
 
 const MainButton = ({ text, handler }: MainButtonProps) => {
   return (
     <button 
       className='rounded-[15px] mt-[2em] px-[40px] py-[15px] bg-black text-[#f1f1f1]'
-      onClick={ handler }
+      onClick={ handler ? handler : () => {} }
     >
       { text }
     </button>
--- src/views/CartListView.tsx
+++ src/views/CartListView.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 import DetailsWrapper from './../components/DetailsWrapper';
 import TitleNavigation from './../components/Navigation/TitleNavigation';
 import ProductCartItem from './../components/Product/ProductCartItem';
 import { IProduct } from '@/interfaces/IProduct';
+import MainButton from '../components/Buttons/MainButton/index';
 
 type CartProps = {
   products: {
@@ -15,22 +17,34 @@ const CartListView = ({ products }: CartProps) => {
     <>
       <DetailsWrapper>
         <TitleNavigation title='Shopping Cart' />
+
         <div className='
           w-full items-center mt-[2.5em] text-[8px] 
           grid gap-[1em] grid-cols-[repeat(1,_1fr)] 
           sm:text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]
         '>
+
           {
-            products.map((item) => {
-              return (
-                <ProductCartItem 
-                  count={ item.count }
-                  product={ item.product }
-                  key={ `cart-item-${item.product.slug}` }              
-                />
-              )
-            })
+            products.length > 0 ? (
+              products.map((item) => {
+                return (
+                  <ProductCartItem 
+                    count={ item.count }
+                    product={ item.product }
+                    key={ `cart-item-${item.product.slug}` }              
+                  />
+                )
+              })
+            ): (
+              <div className='flex flex-wrap flex-col items-center'>
+                <h2 className='bold color-red-200 text-[2em] text-[red]'>No product added</h2>
+                <Link href={'/products'}>
+                  <MainButton text='See products' />
+                </Link>
+              </div>
+            )
           }
+          
         </div>
       </DetailsWrapper>
     </>
